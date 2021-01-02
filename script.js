e7519c615331e1e0009517a02799e87b53f7a96e
Suharevsky <aliksui.ua@mail.ru>
2021-01-02 15:21:35 +0200

    implementing click event for button to get value from input

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = '🤓 Correct Number';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 20;
+
+// console.log(document.querySelector('.guess').value);
+
+// document.querySelector('.guess').value = 23;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number! 😫';
+  }
+});
